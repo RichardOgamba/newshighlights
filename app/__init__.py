6aa7e87a1dcc4e0da6041a62a5626180cc20366b
Richard Ogamba <ogambarichard20@gmail.com>
2018-01-26 18:06:11 +0300

    add configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from .config import DevConfig
 
 #initialising application
 app = Flask(__name__)
+
+#setting up configaration
+app.config.from_object(DevConfig)
 
 from app import views
