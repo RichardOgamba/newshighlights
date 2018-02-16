c020ba2f43e4c8aebfccc3e0eb949ce3c74a823d
Richard Ogamba <ogambarichard20@gmail.com>
2018-02-16 06:21:37 +0300

    add new_source.py file

---
+++ app/source/news_source.py
@@ -0,0 +1,11 @@
+class News_source:
+    '''
+    News_source class to define news class object
+    '''
+
+    def __init__(self, date, title, review, image, views):
+        self.date = date
+        self.title = title
+        self.summary = summary
+        self.image = image
+        self.views = views
