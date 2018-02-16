6131665e820a573f388261f559a7626eabc1cafa
Richard Ogamba <ogambarichard20@gmail.com>
2018-02-16 06:09:16 +0300

    initialize instance folder

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from .config import DevConfig
 
 #initialising application
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config = True)
 
 #setting up configaration
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 from app import views
